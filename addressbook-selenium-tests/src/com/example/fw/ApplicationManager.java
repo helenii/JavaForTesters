00bd4c0a912f9ee230760a427e4ee94bb812a454
helenii <lena.labkovskaya@gmail.com>
2015-07-21 19:22:00 +0300

    Add config files for testng

--- addressbook-selenium-tests/src/com/example/fw/ApplicationManager.java
+++ addressbook-selenium-tests/src/com/example/fw/ApplicationManager.java
@@ -1,10 +1,13 @@
 package com.example.fw;
 
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class ApplicationManager {
 
@@ -14,12 +17,21 @@ public class ApplicationManager {
 	private NavigationHelper navigationHelper;
 	private GroupHelper groupHelper;
 	private ContactHelper contactHelper;
+	private Properties properties;
 
-	public ApplicationManager () {
-		driver = new FirefoxDriver();
-		baseUrl = "http://localhost/";
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get(baseUrl + "/addressbookv4.1.4/index.php");
+	public ApplicationManager (Properties properties) {
+		this.properties = properties;
+		String browser = properties.getProperty("browser");
+		if ("firefox".equals(browser)) {
+			driver = new FirefoxDriver();
+		} else if ("chrome".equals(browser)) {
+			driver = new ChromeDriver();
+		} else {
+			throw new Error("Unsupported browser: " + browser);
+		}
+		baseUrl = properties.getProperty("baseUrl");
+		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(baseUrl);
 	}
 
 	public void stop() {
